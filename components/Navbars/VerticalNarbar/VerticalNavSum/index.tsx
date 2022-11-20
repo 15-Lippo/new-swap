b48ab7308324aeee1ac3e6751a38a9478e38bb63
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2022-11-20 08:45:03 +0100

    Update index.tsx

--- components/Navbars/VerticalNarbar/VerticalNavSum/index.tsx
+++ components/Navbars/VerticalNarbar/VerticalNavSum/index.tsx
@@ -86,7 +86,7 @@ export default function VerticalNarbar() {
       {...props}
     >
       <Flex justifyContent="center" my="8">
-        <Image width={120} src="/multifylogo.png" alt="dexdash logo" />
+        <Image width={120} src="/Lisprocoin logo.png" alt="dexdash logo" />
       </Flex>
 
       <Flex
