dd5a94c26a8873164ef0a28cec530955a7ef9a6c
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2022-11-20 08:28:47 +0100

    Update pools.tsx

--- pages/pools.tsx
+++ pages/pools.tsx
@@ -7,7 +7,7 @@ export default function pools() {
   return (
     <>
       <Head>
-        <title>Multify - Analytics DashDex </title>
+        <title>Lisprocoin - Analytics DashDex </title>
         <meta
           name="description"
           content="Multi-Chain Analytics Defi Dex Dashbord powered by Covalent Api: Built with Nextjs and Chakra UI"
